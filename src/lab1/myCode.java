d2f37fc564723c4ac61864eccd676682cb72e90a
narumonneamwong <narumonneam@gmail.com>
2018-06-26 11:40:03 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class myCode {
+    public static void main(String[] args) {
+
+
+    System.out.println("Hello World.");
+    System.out.println("Narumon Neamwong");
+    System.out.println("361211760017");
+    System.out.println("MIT211");
+    System.out.println("RMUTSV");
+
+    }//main
+
+
+
+
+
+
+
+
+
+
+}//class
